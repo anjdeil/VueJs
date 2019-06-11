f2f77a9a7c5d7754364eda40aca617b35dcd0267
Yana <yanalytvynenko@gmail.com>
2019-06-11 19:24:38 +0200

    study
    ddd

---
+++ my-project/src/Hello.vue
@@ -0,0 +1,25 @@
+ 
+ <template>
+ <div v-bind:class="bgYellow">
+    {{heading}}
+  </div>
+ 
+ </template>
+ <script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      heading: 'Hello World',
+      bgYellow: 'bgYellow'
+    }
+  }
+}
+ </script>
+ 
+ <style>
+.bgYellow{
+  background-color: yellow;
+}
+ 
+ </style>
